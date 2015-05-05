0fa59b050a227f3005d90f650eb28cb64e6e93aa
hugoz <hugo@hugo-zaragoza.net>
2015-05-05 17:43:25 +0200

    report improved

--- Accounting/scripts/report.groovy
+++ Accounting/scripts/report.groovy
@@ -50,42 +50,34 @@ WHERE
 ORDER BY c.type DESC, client_name;'''
 
 
-commands << "Contracts that Ended Recently"
-commands << '''SELECT c.id, c.name, cl.name AS client_name, start, end, c.type FROM contract c  LEFT JOIN clien\
-t cl ON c.client_id=cl.id WHERE DATEDIFF(NOW(),c.end)<7 AND c.end<=CURRENT_DATE()
+commands << "Contracts that ended in the last 30 days"
+commands << '''SELECT c.id, c.name, cl.name AS client_name, start, end, c.type, c.confirmedEnded FROM contract c  LEFT JOIN clien\
+t cl ON c.client_id=cl.id WHERE DATEDIFF(NOW(),c.end)<30 AND c.end<=CURRENT_DATE()
 ORDER BY c.type DESC, client_name;'''
 
 commands << "--- PILOTS"
 
 commands << "Active Pilots:";
 commands << '''
-SELECT  sales_person AS SP, c.id, c.name, c.pilot_length - DATEDIFF(CURRENT_DATE(), c.start) AS daysReamining, COUNT(c.id) AS '#profiles'
+SELECT  sales_person AS SP, c.id, c.name, c.pilot_length - DATEDIFF(CURRENT_DATE(), c.start) AS daysReamining, COUNT(c.id) AS '#profiles',
+     GROUP_CONCAT(profile_id, ':', p.name) AS profiles 
   FROM profiles p LEFT JOIN contract c ON p.contract_id=c.id
   WHERE (c.type='internal' OR c.type='pilot')
     AND ( c.confirmedClosed IS NULL )
   GROUP BY c.id ORDER BY sales_person, daysReamining, c.name;
 '''
 
-commands << "--- CONTRACTS IN GENERAL"
-
-
-commands << "Active Contracts"
-commands << '''
-SELECT  c.id, c.name, cl.name AS client_name, start, c.type FROM contract c  LEFT JOIN client cl\
- ON c.client_id=cl.id WHERE c.start < NOW() AND ( ( c.end IS NULL ) OR (c.end > NOW()) )
- ORDER BY c.type DESC, client_name;
-'''
 
 commands << "---- DB ADMIN WARNINGS:"
 
-commands << "PROBLEMS: Conatracts ended but not confirmed"
-commands << "SELECT  c.id,c.name FROM contract c WHERE DATEDIFF(CURRENT_DATE(),c.end)>=0 AND c.end IS NOT NULL AND c.confirmedClosed IS NULL"
+commands << "PROBLEMS: Conatracts missing end confirmation:"
+commands << "SELECT  c.id,c.name, c.start, c.end, c.dataAccessEnd FROM contract c WHERE  c.end<CURRENT_DATE() AND c.confirmedClosed IS NULL AND (c.dataAccessEnd IS NULL OR c.dataAccessEnd<CURRENT_DATE() )"
 
 commands << "PROBLEMS: Contracts missing main_profile_id"
 commands << "SELECT  c.id,c.name FROM contract c WHERE main_profile_id IS NULL"
 
-commands << "PROBLEMS: Profiles missing contract_id"
-commands << "SELECT  p.profile_id, p.name, deleted FROM profiles p WHERE contract_id IS NULL AND deleted != 1"
+commands << "PROBLEMS: Active profiles missing contract_id"
+commands << "SELECT  p.profile_id, p.name FROM profiles p WHERE contract_id IS NULL AND deleted != 1"
 
 commands << "Profiles that are ACTIVE but DO NOT HAVE a contract:"
 commands << '''
@@ -95,6 +87,15 @@ WHERE
   AND ( NOT EXISTS( SELECT * FROM contract c WHERE c.confirmedClosed IS NULL AND p.contract_id=c.id) )
   order by c.name, p.created DESC
 '''
+commands << "--- ALL ACTIVE CONTRACTS"
+
+commands << "Active Contracts"
+commands << '''
+SELECT  c.id, c.name, cl.name AS client_name, start, c.type FROM contract c  LEFT JOIN client cl\
+ ON c.client_id=cl.id WHERE c.start < NOW() AND ( ( c.end IS NULL ) OR (c.end > NOW()) )
+ ORDER BY c.type DESC, client_name;
+'''
+
 
 
 // -----------------------
